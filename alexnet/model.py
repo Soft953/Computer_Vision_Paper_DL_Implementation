aa8ed9f1b06be2c453752c28155e86cdfe6b4a3a
Sofiene Bourghes <sofiene.bourghes@epita.fr>
2021-01-21 16:21:36 +0100

    adapt model to data

--- alexnet/model.py
+++ alexnet/model.py
@@ -7,28 +7,43 @@ class AlexNet(nn.Module):
     def __init__(self, n_classes):
         super(AlexNet, self).__init__()
 
-        self.conv1 = nn.Conv2d(3, 96, kernel_size=11, stride=4)
-        self.conv2 = nn.Conv2d(96, 256, kernel_size=5)
-        self.conv3 = nn.Conv2d(256, 384, kernel_size=3)
-        self.conv4 = nn.Conv2d(384, 384, kernel_size=3)
-        self.conv5 = nn.Conv2d(384, 256, kernel_size=3)
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=1) #11 with stride 4
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=1) #5
+        self.conv3 = nn.Conv2d(64, 256, kernel_size=1)
+        #self.conv4 = nn.Conv2d(256, 512, kernel_size=1)
+        #self.conv5 = nn.Conv2d(512, 256, kernel_size=1)
 
         self.drop = nn.Dropout(0.5)
         self.norm = nn.LocalResponseNorm(size=5, alpha=0.0001, beta=0.75, k=2)
-        self.pool = nn.MaxPool2d(kernel_size=3, stride=2)
+        self.pool = nn.MaxPool2d(kernel_size=3)
 
-        self.fc1 = nn.Linear(0, 4096)
-        self.fc2 = nn.Linear(4096, 4096)
-        self.fc3 = nn.Linear(4096, n_classes)
+        self.flatten = nn.Flatten()
+
+        self.fc1 = nn.Linear(256, 1024)
+        self.fc2 = nn.Linear(1024, 512)
+        self.fc3 = nn.Linear(512, n_classes)
 
     def forward(self, x):
+        print("start shape:", x.shape)
         x = self.pool(F.relu(self.norm(self.conv1(x))))
+        print("shape conv1:", x.shape)
         x = self.pool(F.relu(self.norm(self.conv2(x))))
-        x = self.pool(F.relu(self.norm(self.conv3(x))))
-        x = self.pool(F.relu(self.norm(self.conv4(x))))
-        x = self.pool(F.relu(self.norm(self.conv5(x))))
+        print("shape conv2:", x.shape)
 
-        x = x.view(x.size(0), -1)
+        #print(x.shape)
+        x = self.conv3(x)
+        #print(x.shape)
+        x = self.norm(x)
+        #print(x.shape)
+        x = F.relu(x)
+        #print(x.shape)
+        x = self.pool(x)
+        #print(x.shape)
+
+        #x = self.pool(F.relu(self.norm(self.conv4(x))))
+        #x = self.pool(F.relu(self.norm(self.conv5(x))))
+
+        x = self.flatten(x)
 
         x = self.drop(F.relu(self.fc1(x)))
         x = self.drop(F.relu(self.fc2(x)))
